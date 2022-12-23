8fae91606218d7572294ea603799577786fe20ee
Michael Brown <redhoodie@gmail.com>
2022-12-24 11:15:33 +1300

    feat: add articles scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  get '/articles', to: 'articles#index'
+  resources :articles
 
-  root 'articles#index'
+  # Defines the root path route ("/")
+  root to: redirect('/articles')
 end
